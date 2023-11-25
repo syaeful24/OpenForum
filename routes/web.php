645a6a3e4274f5f308c49892d8919a6fdedf01fd
syaeful24 <work.syaeful@gmail.com>
2023-11-25 22:36:35 +0700

    Login Feature

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Register;
+use App\Http\Controllers\Login;
+use App\Http\Controllers\Main;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,4 +23,10 @@ Route::get('/', function () {
 Route::middleware('guest')->group(function () {
     Route::get('/register', [Register::class, 'index'])->name('register');
     Route::post('/register/validate', [Register::class, 'register']);
+    Route::get('/login', [Login::class, 'index'])->name('login');
+    Route::post('/login/validate', [Login::class, 'login']);
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [Main::class, 'dashboard'])->name('dashboard');
 });
