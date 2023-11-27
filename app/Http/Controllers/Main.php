564a112401b624f2a82037b11f63fb18ab92b9f3
syaeful24 <work.syaeful@gmail.com>
2023-11-27 18:51:02 +0700

    Post update

--- app/Http/Controllers/Main.php
+++ app/Http/Controllers/Main.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use App\Models\User;
 class Main extends Controller
 {
     public function dashboard(){
         $userData = Auth::user();
-        return view("dashboard", ['user' => $userData]);
+        $post = $userData->posts()->latest()->paginate(5);
+        return view("dashboard", ['user' => $userData, 'posts' => $post]);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,4 +2,14 @@
     Logged in as {{$user->name}}
 </div>
 
-@include('post.create');
+<div>
+    <h1>Your Post</h1>
+    @foreach ($posts as $post)
+    <div>
+        <h3>{{$post->title}}</h3>
+        <p>{{$post->content}}</p>
+        <h6>{{$post->created_at}}</h6>
+    </div> 
+    @endforeach
+</div>
+
