24b657fae312813e7f071d5e5a375e945255ee91
syaeful24 <work.syaeful@gmail.com>
2023-11-29 16:28:09 +0700

    Tailwind

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts/main')
+@section('content')
+<div>
+    <h1>Newest Post</h1>
+    {{$posts}}
+    @foreach ($posts as $p)
+        <p>{{$p->title}} by {{$p->user->name}} at {{$p->created_at->format('D, d M Y')}}</p>
+    @endforeach
+</div>
+@endsection
